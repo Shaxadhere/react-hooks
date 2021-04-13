e277f652fcb66d54236743ec9b7c3e866030261f
Shaxadhere <Shaxad.here@gmail.com>
2021-04-13 11:31:20 +0500

    react news app

--- src/App.js
+++ src/App.js
@@ -3,23 +3,37 @@ import React, {Component, useState, useEffect} from 'react'
 const App = () => {
   const [news, setNews] = useState([])
   const [searchQuery, setSearchQuery] = useState('react')
+  const [url, setUrl] = useState('https://hn.algolia.com/api/v1/search?query=react')
+  const [loading, setLoading] = useState(false)
+
   const fetchNews = () => {
+    setLoading(true)
     fetch(`https://hn.algolia.com/api/v1/search?query=${searchQuery}`)
     .then(result => result.json())
-    .then(data => setNews(data.hits))
+    .then(data => (setNews(data.hits), setLoading(false)))
     .catch(error => console.log(error))    
   }
+
   useEffect(() => {
     fetchNews()
-  }, [searchQuery])
+  }, [url])
+
   const handleChange = (e) => {
     setSearchQuery(e.target.value)
   }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setUrl(`https://hn.algolia.com/api/v1/search?query=${searchQuery}`)
+  }
+
   return (
     <div>
       <h2>News</h2>
-      <form>
+      {loading ? <h2>Loading</h2> : ""}
+      <form onSubmit={handleSubmit}>
         <input type="text" value={searchQuery} onChange={handleChange} />
+        <input type="submit" value="Search"/>
       </form>
       {news.map((n, i) => (<p key={i}>{n.title}</p>))}
     </div>

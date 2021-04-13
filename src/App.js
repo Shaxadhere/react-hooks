e8de66d3424f447dbc724b5d224fd1a501d383d2
Shaxadhere <Shaxad.here@gmail.com>
2021-04-13 10:21:32 +0500

    react hooks

--- src/App.js
+++ src/App.js
@@ -1,22 +1,72 @@
-import React, {Component} from 'react'
+import React, {Component, useState, useEffect} from 'react'
 
-class App extends Component{
-  state = {
-    count: 0
+const App = () => {
+  const [news, setNews] = useState([])
+  
+  const fetchNews = () => {
+    fetch('https://hn.algolia.com/api/v1/search?query=react')
+    .then(result => result.json())
+    .then(data => setNews(data.hits))
+    .catch(error => console.log(error))    
   }
-  increment = () => {
-    this.setState({
-      count: this.state.count + 1
-    })
-  }
-  render(){
-    return (
-      <div>
-        <h2>Counter App</h2>
-        <button onClick={this.increment}>Clicked {this.state.count} times</button>
-      </div>
-    )
-  }
+  useEffect(() => {
+    fetchNews()
+  })
+  return (
+    <div>
+      <h2>News</h2>
+      {news.map((n, i) => {<p key={i}>{n.title}</p>})}
+    </div>
+  )
 }
+
+
+// const App = () => {
+//     const [count, setCount] = useState(0)
+//     useEffect(() => {
+//       document.title = `Clicked ${count} times`
+//     })
+//     const increment = () => {
+//         setCount(count + 1)
+//     }
+//     return (
+//         <div>
+//             <h2>Counter App</h2>
+//             <button onMouseOver={increment}
+//                 onClick={increment}>Clicked {count}
+//                 times</button>
+//         </div>
+//     )
+// }
+
+// class App extends Component {
+//     state = {
+//         count: 0
+//     }
+//     increment = () => {
+//         this.setState({
+//             count: this.state.count + 1
+//         })
+//     }
+//     componentDidMount(){
+//       document.title = `Clicked ${this.state.count} times`
+//     }
+//     componentDidUpdate(){
+//       document.title = `Clicked ${this.state.count} times`
+//     }
+//     render() {
+//         return (
+//             <div>
+//                 <h2>Counter App</h2>
+//                 <button onClick={
+//                     this.increment
+//                 }>Clicked {
+//                     this.state.count
+//                 }
+//                     times</button>
+//             </div>
+//         )
+//     }
+// }
 
 export default App;

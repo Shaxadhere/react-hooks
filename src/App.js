51694a98a1983260bafa380a733c9645199ff28a
Shaxadhere <Shaxad.here@gmail.com>
2021-04-13 11:14:42 +0500

    news app

--- src/App.js
+++ src/App.js
@@ -2,20 +2,26 @@ import React, {Component, useState, useEffect} from 'react'
 
 const App = () => {
   const [news, setNews] = useState([])
-  
+  const [searchQuery, setSearchQuery] = useState('react')
   const fetchNews = () => {
-    fetch('https://hn.algolia.com/api/v1/search?query=react')
+    fetch(`https://hn.algolia.com/api/v1/search?query=${searchQuery}`)
     .then(result => result.json())
     .then(data => setNews(data.hits))
     .catch(error => console.log(error))    
   }
   useEffect(() => {
     fetchNews()
-  })
+  }, [searchQuery])
+  const handleChange = (e) => {
+    setSearchQuery(e.target.value)
+  }
   return (
     <div>
       <h2>News</h2>
-      {news.map((n, i) => {<p key={i}>{n.title}</p>})}
+      <form>
+        <input type="text" value={searchQuery} onChange={handleChange} />
+      </form>
+      {news.map((n, i) => (<p key={i}>{n.title}</p>))}
     </div>
   )
 }
